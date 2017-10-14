0999a23d3c5a775eeb69c3d2a20f44174fb6f8dd
Kandyce Burks <kburks2@uic.edu>
2017-10-14 17:24:30 -0500

    Update CS362_kburks2_Lab4.ino

--- CS362_kburks2_Lab4.ino
+++ CS362_kburks2_Lab4.ino
@@ -18,23 +18,30 @@ void loop() {
 
 void displayBrightness(int value) {
   lcd.setCursor(0, line1);
+  
   if (value <= 205) {
     lcd.print("Dark");
   }
+  
   else if (value <= 410) {
     lcd.print("Partially Dark");
   }
+  
   else if (value <= 615) {
     lcd.print("Medium");
   }
+  
   else if (value <= 820) {
     lcd.print("Partially Light");
   }
+  
   else if (value <= 1023) {
     lcd.print("Fully Lit");
   }
+  
   lcd.setCursor(0, line1);
   delay(500);
+  
   for (itr  = 0; itr < 16; itr++) {
     lcd.print(" ");
   }
